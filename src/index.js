ef22444f8f47d8096d7308a65ebc0d0d38e30233
administrator <liquidnuker@users.noreply.github.com>
2017-02-12 10:29:36 -0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,64 @@
+// import "./js/vendor/vue.min.js";
+// import Vue from 'vue';
+// var Vue = require("./js/vendor/vue.min.js"); // ok
+let Vue = require("./js/vendor/vue.min.js");
+let app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: "v-bind message"
+  }
+});
+
+// 
+// ======================================================/
+var store = {
+  debug: true,
+  state: {
+    message: 'Hello!'
+  },
+  getStoreMessage () {
+    return this.state.message;
+  },
+  setStoreMessage (newValue) {
+    this.state.message = newValue;
+  },
+  clearStoreMessage () {
+    this.state.message = '';
+  }
+};
+
+var vmA = new Vue({
+  el: '#app1',
+  data: {
+    privateState: {},
+    sharedState: store.state,
+    message: ''
+  },
+  methods: {
+    test: function() {
+      this.message = this.sharedState.message;
+    }
+  }
+});
+
+var vmB = new Vue({
+  el: '#app2',
+  data: {
+    privateState: {},
+    sharedState: store.state,
+    message: ''
+  },
+  methods: {
+    test: function() {
+      this.message = this.sharedState.message;      
+    }
+  }
+});
+
+vmA.test();
+vmB.test();
+
+// todo:
+// ======================================================/
+// add functions for set/clear/retrieve
+
