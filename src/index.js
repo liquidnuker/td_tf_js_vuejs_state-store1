e026c8a2310f825f11be186d0f8e8c038e17cb03
administrator <liquidnuker@users.noreply.github.com>
2017-03-23 19:45:27 -0800

    removed unused lines

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// import "./js/vendor/vue.min.js";
-// import Vue from 'vue';
-// var Vue = require("./js/vendor/vue.min.js"); // ok
 let Vue = require("./js/vendor/vue.min.js");
 let app2 = new Vue({
   el: '#app-2',
